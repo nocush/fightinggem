570429ae954c55753f9198a49bf4196daaaf0ba7
Mat <106345025+nocush@users.noreply.github.com>
2022-10-01 22:45:09 +0200

    timers done :)

--- index.js
+++ index.js
@@ -111,29 +111,38 @@ function rectangularCollision({rectangle1, rectangle2}){
     return (rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width
         && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height)
 }
-let timer = 20
+
+function determineWinner({player, enemy, timerId}){
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    }else if(player.health > enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Player #1 wins'
+    }else if(player.health < enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Player #2 wins'
+    }
+    window.cancelAnimationFrame(animationId)
+}
+
+let timer = 60
+let timerId
+let animationId
 function decreaseTimer(){
     if (timer>0) {
-        setTimeout(decreaseTimer, 1000)
+        timerId = setTimeout(decreaseTimer, 1000)
         timer--
         document.querySelector('#timer').innerHTML = timer
     }
 
     if (timer === 0){
-        document.querySelector('#displayText').style.display = 'flex'
-        if (player.health === enemy.health){
-            document.querySelector('#displayText').innerHTML = 'Tie'
-        }else if(player.health > enemy.health){
-            document.querySelector('#displayText').innerHTML = 'Player #1 wins'
-        }else if(player.health < enemy.health){
-            document.querySelector('#displayText').innerHTML = 'Player #2 wins'
-        } 
+        determineWinner({player, enemy, timerId})
     }
 }
 
 decreaseTimer()
 function animate(){
-    window.requestAnimationFrame(animate)
+    animationId = window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
@@ -166,7 +175,12 @@ function animate(){
         enemy.isAttacking = false
         player.health -= 20
             document.querySelector('#playerHealth').style.width = player.health + "%"
-}
+    }
+
+    //end game based on health
+    if(enemy.health <= 0 || player.health <= 0) {
+        determineWinner({player, enemy, timerId})
+    }
 }
 
 animate()
